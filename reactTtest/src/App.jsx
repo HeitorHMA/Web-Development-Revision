f08fec0d64a4228477ef52a9c3f441417a191a16
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-08-24 17:55:02 -0300

    use export default

--- reactTtest/src/App.jsx
+++ reactTtest/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } = require("react")
+import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  function augment() {
+    setCounter(counter + 1);
+  }
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Counter: {counter}</h1>
+      <button onClick={augment}>Augment</button>
+    </div>
+  );
 }
-function augment(){
-  setCounter(counter+1)
-}
-return(
-  <div style={{ padding: '20px'}}>
-  <h1>Counter: {counter}</h1>
-  <button onClick={augment}>Augment</button>
-  </div>
-);
+
+export default App;
