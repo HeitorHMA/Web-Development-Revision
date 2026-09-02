dd61302af7ed00c3a11a3f75c81f29717c36ebf7
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-09-02 06:36:29 -0300

    feat: introduce checking and variable to input box

--- reactTtest/src/App.jsx
+++ reactTtest/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
     <div style={{ padding: "20px" }}>
       <h1>Input</h1>
       <input placeholder="Write here" onChange={setInfo} value={input} />
-      <h2>What you wrote was: {input}</h2>
+      {input.length === 0 && <h2>Write Something</h2>}
+      {input.length > 0 && <h2>What you wrote was: {input}</h2>}
     </div>
   );
 }
