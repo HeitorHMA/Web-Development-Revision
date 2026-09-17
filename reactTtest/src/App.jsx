f03456dabf248caa56cae21d61e2f0af4529d072
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-09-17 16:06:47 -0300

    create a system that uses .map() to show arrays

--- reactTtest/src/App.jsx
+++ reactTtest/src/App.jsx
@@ -2,15 +2,28 @@ import { useState } from "react";
 
 function App() {
   const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
   function setInfo(e) {
     setInput(e.target.value);
   }
+  function setItem() {
+    setList([...list, input]);
+    setInput("");
+  }
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Input</h1>
-      <input placeholder="Write here" onChange={setInfo} value={input} />
       {input.length === 0 && <h2>Write Something</h2>}
-      {input.length > 0 && <h2>What you wrote was: {input}</h2>}
+      <input placeholder="Write here" onChange={setInfo} value={input} />
+
+      <button onClick={setItem}>Add</button>
+      {list.length > 0 && <h2>What you wrote was:</h2>}
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
