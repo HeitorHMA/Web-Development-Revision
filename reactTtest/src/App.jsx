131b41d4c973567a16abc7234ffae67422ca109b
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-09-17 16:07:20 -0300

    change name

--- reactTtest/src/App.jsx
+++ reactTtest/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>To do List</h1>
-      {input.length === 0 && <h2>Write Something</h2>}
+      {input.length === 0 && <h2>Write an goal</h2>}
       <input placeholder="Write here" onChange={setInfo} value={input} />
 
       <button onClick={setItem}>Add</button>
