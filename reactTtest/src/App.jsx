879b6d2b5534ac415ed2522a3d4c34950a0cb262
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-09-23 08:06:59 -0300

    try to add new button

--- reactTtest/src/App.jsx
+++ reactTtest/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
     setList([...list, input]);
     setInput("");
   }
+  function deleteItem(indexToDelete) {
+    const newList = list.filter((item, index) => index != indexToDelete);
+    setList(newList);
+  }
 
   return (
     <div style={{ padding: "20px" }}>
@@ -21,7 +25,10 @@ function App() {
       {list.length > 0 && <h2>Your goals are:</h2>}
       <ul>
         {list.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteItem(index)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
